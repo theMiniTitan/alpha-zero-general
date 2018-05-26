06eb71e52ff5ef03e7f5b00242504dc9cf2efad6
Brendan <brendan.rogerz@gmail.com>
2018-05-27 03:46:44 +1000

    rewrote MCTS

--- MCTS.py
+++ MCTS.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
+
 EPS = 1e-8
+
 
 class MCTS():
     """
@@ -11,13 +13,13 @@ class MCTS():
         self.game = game
         self.nnet = nnet
         self.args = args
-        self.Qsa = {}       # stores Q values for s,a (as defined in the paper)
-        self.Nsa = {}       # stores #times edge s,a was visited
-        self.Ns = {}        # stores #times board s was visited
-        self.Ps = {}        # stores initial policy (returned by neural net)
+        self.QValue = {}  # stores Q values for s,a (as defined in the paper)
+        self.NStateAction = {}  # stores #times edge s,a was visited
+        self.NState = {}  # stores #times board s was visited
+        self.nnetPolicy = {}  # stores initial policy (returned by neural net)
 
-        self.Es = {}        # stores game.getGameEnded ended for board s
-        self.Vs = {}        # stores game.getValidMoves for board s
+        self.Es = {}  # stores game.getGameEnded ended for board s
+        self.validMoves = {}  # stores game.getValidMoves for board s
 
     def getActionProb(self, canonicalBoard, temp=1):
         """
@@ -28,22 +30,27 @@ class MCTS():
             probs: a policy vector where the probability of the ith action is
                    proportional to Nsa[(s,a)]**(1./temp)
         """
+
         for i in range(self.args.numMCTSSims):
             self.search(canonicalBoard)
 
-        s = self.game.stringRepresentation(canonicalBoard)
-        counts = [self.Nsa[(s,a)] if (s,a) in self.Nsa else 0 for a in range(self.game.getActionSize())]
+        boardState = self.game.stringRepresentation(canonicalBoard)
+        counts = []
+        for action in range(self.game.getActionSize):
+            if(boardState, action) in self.NStateAction:
+                counts.append(self.NStateAction[(boardState, action)])
+            else:
+                counts.append(0)
 
-        if temp==0:
-            bestA = np.argmax(counts)
-            probs = [0]*len(counts)
-            probs[bestA]=1
-            return probs
+        if temp == 0:
+            bestAction = np.argmax(counts)
+            moveProbabilities = [0] * len(counts)
+            moveProbabilities[bestAction] = 1
+            return moveProbabilities
 
         counts = [x**(1./temp) for x in counts]
-        probs = [x/float(sum(counts)) for x in counts]
+        probs = [x/float(sum(counts))for x in counts]
         return probs
-
 
     def search(self, canonicalBoard):
         """
@@ -64,65 +71,56 @@ class MCTS():
         Returns:
             v: the negative of the value of the current canonicalBoard
         """
+        boardState = self.game.stringRepresentation(canonicalBoard, 1)
 
-        s = self.game.stringRepresentation(canonicalBoard)
+        # check if the boardstate has been seem before
+        if boardState not in self.Es:
+            # note we've seen this board before
+            self.Es[boardState] = self.game.getGameEnded(canonicalBoard, 1)
+        if self.Es[boardState] != 0:  # game has ended
+            return -self.Es[boardState]
 
-        if s not in self.Es:
-            self.Es[s] = self.game.getGameEnded(canonicalBoard, 1)
-        if self.Es[s]!=0:
-            # terminal node
-            return -self.Es[s]
-
-        if s not in self.Ps:
+        if boardState not in self.nnetPolicy:
             # leaf node
-            self.Ps[s], v = self.nnet.predict(canonicalBoard)
+            self.nnetPolicy[boardState], v = self.nnet.predict(canonicalBoard)
             valids = self.game.getValidMoves(canonicalBoard, 1)
-            self.Ps[s] = self.Ps[s]*valids      # masking invalid moves
-            sum_Ps_s = np.sum(self.Ps[s])
-            if sum_Ps_s > 0:
-                self.Ps[s] /= sum_Ps_s    # renormalize
-            else:
-                # if all valid moves were masked make all valid moves equally probable
-                
-                # NB! All valid moves may be masked if either your NNet architecture is insufficient or you've get overfitting or something else.
-                # If you have got dozens or hundreds of these messages you should pay attention to your NNet and/or training process.   
-                print("All valid moves were masked, do workaround.")
-                self.Ps[s] = self.Ps[s] + valids
-                self.Ps[s] /= np.sum(self.Ps[s])
+            self.nnetPolicy[boardState] = self.nnetPolicy[boardState] * valids  # only want valid moves
+            # check if move vector is normalised
+            sum_Ps_s = np.sum(self.nnetPolicy[boardState])
 
-            self.Vs[s] = valids
-            self.Ns[s] = 0
+            self.nnetPolicy[boardState] /= sum_Ps_s  # renormalize
+            self.validMoves[boardState] = valids
+            self.NState[boardState] = 0
             return -v
 
-        valids = self.Vs[s]
-        cur_best = -float('inf')
-        best_act = -1
+        validMoves = self.validMoves[boardState]
+        curBest = -float('inf')
+        bestAction = 1
 
-        # pick the action with the highest upper confidence bound
-        for a in range(self.game.getActionSize()):
-            if valids[a]:
-                if (s,a) in self.Qsa:
-                    u = self.Qsa[(s,a)] + self.args.cpuct*self.Ps[s][a]*math.sqrt(self.Ns[s])/(1+self.Nsa[(s,a)])
+        # find which action is best
+        for action in range(self.game.getActionSize()):
+            if validMoves[action]:
+                if (boardState, action) in self.QValue:
+                    u = self.QValue[(boardState, action)] + self.args.cpuct * self.nnetPolicy[boardState][
+                        action] * math.sqrt(self.NState[boardState]) / (1 + self.NStateAction[(boardState, action)])
                 else:
-                    u = self.args.cpuct*self.Ps[s][a]*math.sqrt(self.Ns[s] + EPS)     # Q = 0 ?
+                    u = self.args.cpuct * self.nnetPolicy[boardState][action] * math.sqrt(self.NState[boardState] + EPS)
+            if u > curBest:
+                curBest = u
+                bestAction = action
 
-                if u > cur_best:
-                    cur_best = u
-                    best_act = a
+        nextState, nextPlayer = self.game.getNextState(canonicalBoard, 1, bestAction)
+        nextState = self.game.getCanonicalForm(nextState, nextPlayer)
 
-        a = best_act
-        next_s, next_player = self.game.getNextState(canonicalBoard, 1, a)
-        next_s = self.game.getCanonicalForm(next_s, next_player)
+        v = self.search(nextState)
 
-        v = self.search(next_s)
-
-        if (s,a) in self.Qsa:
-            self.Qsa[(s,a)] = (self.Nsa[(s,a)]*self.Qsa[(s,a)] + v)/(self.Nsa[(s,a)]+1)
-            self.Nsa[(s,a)] += 1
-
+        if (boardState, bestAction) in self.QValue:
+            self.QValue[(boardState, bestAction)] = (self.NStateAction[(boardState, bestAction)] * self.QValue[
+                (boardState, bestAction)] + v) / (self.NStateAction[(boardState, bestAction)] + 1)
+            self.NStateAction[(boardState, bestAction)] += 1
         else:
-            self.Qsa[(s,a)] = v
-            self.Nsa[(s,a)] = 1
+            self.QValue[(boardState, bestAction)] = v
+            self.NStateAction[(boardState, bestAction)] = 1
 
-        self.Ns[s] += 1
+        self.NState[boardState] += 1
         return -v
